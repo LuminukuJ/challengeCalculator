22ec284b9fa8d94c1cae80409f91e7e431a19e83
Jonathan LUMINUKU <luminuku.pro@hotmail.com>
2018-10-26 09:56:17 +0200

    Refactoring, fusion des boucles if pour la visibilité.

--- calculator/src/com/socgen/calculator/App.java
+++ calculator/src/com/socgen/calculator/App.java
@@ -10,57 +10,42 @@ public class App {
     public static void main(String[] args) {
   
         String str = args[0];
-        double[] tab = new double[2];
-        String[] tab2 = new String[2];
-        String ope;
+        String[] tab = new String[2];
+        
         double resultat;
-
+        
         if (str.indexOf("-") >= 0) {
-            tab2 = str.split("-");
-            ope = "-";
+            tab = str.split("-");
+            Double op1 = Double.valueOf(tab[0]);
+            Double op2 = Double.valueOf(tab[1]);
+            resultat = Operator.SUBTRACT.operation(op1, op2);
         }
         else if (str.indexOf("/") > 0) {
-            tab2 = str.split("/");
-            ope = "/";
+            tab = str.split("/");
+            Double op1 = Double.valueOf(tab[0]);
+            Double op2 = Double.valueOf(tab[1]);
+            resultat = Operator.DIVIDE.operation(op1, op2);
         }
         else if (str.indexOf("+") > 0) {
-            tab2 = str.split("\\+");
-            ope = "+";
+            tab = str.split("\\+");
+            Double op1 = Double.valueOf(tab[0]);
+            Double op2 = Double.valueOf(tab[1]);
+            resultat = Operator.ADD.operation(op1, op2);
         }
         else if (str.indexOf("%") > 0) {
-            tab2 = str.split("%");
-            ope = "%";
+            tab = str.split("%");
+            Double op1 = Double.valueOf(tab[0]);
+            Double op2 = Double.valueOf(tab[1]);
+            resultat = Operator.MODULO.operation(op1, op2);
         }
         else {
-            tab2 = str.split("\\*");
-            ope = "*";
+            tab = str.split("\\*");
+            Double op1 = Double.valueOf(tab[0]);
+            Double op2 = Double.valueOf(tab[1]);
+            resultat = Operator.MULTIPLY.operation(op1, op2);
         }
-
-            tab[0] = Double.parseDouble(tab2[0]);
-            tab[1] = Double.parseDouble(tab2[1]);
-            
-            double x =  tab[0];
-            double y =  tab[1];
-            
-        if (ope == "+") {
-        	resultat = Operator.ADD.operation(x, y);
-        	 System.out.println(resultat);
-        }
-        else if (ope == "-") {
-        	resultat = Operator.SUBTRACT.operation(x, y);
-       	 System.out.println(resultat);
-        }
-        else if (ope == "*") {
-        	resultat = Operator.MULTIPLY.operation(x, y);
-       	 System.out.println(resultat);
-        }
-        else if (ope == "/") {
-        	resultat = Operator.DIVIDE.operation(x, y);
-       	 System.out.println(resultat);
-        }
-        else {
-        	resultat = Operator.MODULO.operation(x, y);
-       	 System.out.println(resultat);
-        }
+        
+        System.out.println(resultat);
+        
     }   
 }
